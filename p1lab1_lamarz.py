1988278010e7916eef5011db4f80d3d02c5696ef
zlamar760 <zlamar760@gmail.com>
2024-09-19 10:28:51 -0400

    P1LAB1 v1

---
+++ p1lab1_lamarz.py
@@ -0,0 +1,13 @@
+#Zachary LaMar
+#9/17/24
+#P1Lab1
+#Prompt the user to enter their name and welcome them to class.
+
+#Ask for first name
+fName=input("Enter your first name: ")
+
+#Ask for last name
+lName=input("Enter your last name: ")
+
+#Display first and last name in a string
+print("Hello,",fName,lName+"!","Welcome to CTI-110")
